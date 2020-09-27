a899f08995fec8d9bd78eca58264f88d28ab3666
Santiago <santiago.rodas1@correo.icesi.edu.co>
2020-09-27 11:51:52 -0500

    New test

---
+++ Codigo/Headquarter/test/model/TestBank.java
@@ -0,0 +1,20 @@
+/*
+ * UNIVERSIDAD ICESI
+ * TAREA INTEGRADORA 1 - ESTRUCTURAS DE DATOS
+ * RODAS / DIAZ / MARTINEZ
+ */
+
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestBank {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Codigo/Headquarter/test/model/TestCurrentAccount.java
@@ -0,0 +1,20 @@
+/*
+ * UNIVERSIDAD ICESI
+ * TAREA INTEGRADORA 1 - ESTRUCTURAS DE DATOS
+ * RODAS / DIAZ / MARTINEZ
+ */
+
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCurrentAccount {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
