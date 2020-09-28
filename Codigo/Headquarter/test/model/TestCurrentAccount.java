0289f17d809e864bb1efb04388a498761e1d08eb
Santiago <santiago.rodas1@correo.icesi.edu.co>
2020-09-28 14:37:40 -0500

    New model test

--- Codigo/Headquarter/test/model/TestCurrentAccount.java
+++ Codigo/Headquarter/test/model/TestCurrentAccount.java
@@ -6,15 +6,100 @@
 
 package model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 class TestCurrentAccount {
+	
+	//------------------------------------------------------------------------------------
+	
+	//Relations
+	
+	CurrentAccount currentAccount;
+	
+	//------------------------------------------------------------------------------------
+	
+	//Setup1
+	
+	void setup1() {
+		
+		currentAccount = new CurrentAccount("123456789");
+		
+	}
+	
+	//------------------------------------------------------------------------------------
+	
+	//Test pay
+	
+	@Test
+	void testPay1() {
+		
+		setup1();
+		
+		assertFalse(currentAccount.pay(1000));
+		
+	}
+	
+	//****************************************
+	
+	@Test
+	void testPay2() {
+		
+		setup1();
+		
+		assertTrue(currentAccount.pay(-1000));
+		
+	}
+	
+	//****************************************
+	
+	@Test
+	void testPay3() {
+		
+		setup1();
+		
+		assertFalse(currentAccount.pay(0));
+		
+	}
+	
+	//------------------------------------------------------------------------------------
+	
+	//Test use
+	
+	@Test
+	void testUse1() {
+		
+		setup1();
+		
+		assertTrue(currentAccount.use(1000));
+		
+	}
+	
+	//****************************************
+	
+	@Test
+	void testUse2() {
+		
+		setup1();
+		
+		assertTrue(currentAccount.use(-1000));
+		
+	}
+	
+	//****************************************
+	
+	@Test
+	void testUse3() {
+		
+		setup1();
+		
+		assertTrue(currentAccount.use(0));
+		
+	}
+	
+	//------------------------------------------------------------------------------------
 
-//	@Test
-//	void test() {
-//		fail("Not yet implemented");
-//	}
 
 }
--- Codigo/Headquarter/test/model/TestSavingsAccount.java
+++ Codigo/Headquarter/test/model/TestSavingsAccount.java
@@ -6,15 +6,98 @@
 
 package model;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import org.junit.jupiter.api.Test;
 
 class TestSavingsAccount {
-
-//	@Test
-//	void test() {
-//		fail("Not yet implemented");
-//	}
+	
+	//------------------------------------------------------------------------------------
+	
+	//Relations
+	
+	SavingsAccount savingsAccount;
+	
+	//------------------------------------------------------------------------------------
+	
+	//Setup 1
+	
+	void setup1() {
+		
+		savingsAccount = new SavingsAccount("123456789");
+		
+	}
+	
+	//------------------------------------------------------------------------------------
+	
+	//Test not credit
+	
+	@Test
+	void testDebit1() {
+		
+		setup1();
+		
+		assertFalse(savingsAccount.debit(1000));
+		
+	}
+	
+	//****************************************
+	
+	@Test
+	void testDebit2() {
+		
+		setup1();
+		
+		assertTrue(savingsAccount.debit(-1000));
+		
+	}
+	
+	//****************************************
+	
+	@Test
+	void testDebit3() {
+		
+		setup1();
+		
+		assertFalse(savingsAccount.debit(0));
+		
+	}
+	
+	//------------------------------------------------------------------------------------
+	
+	//Test credit
+	
+	@Test
+	void testCredit1() {
+		
+		setup1();
+		
+		assertTrue(savingsAccount.credit(1000));
+		
+	}
+	
+	//****************************************
+	
+	@Test
+	void testCredit2() {
+		
+		setup1();
+				
+		assertTrue(savingsAccount.debit(-1000));
+		
+	}
+	
+	//****************************************
+	
+	@Test
+	void testCredit3() {
+		
+		setup1();
+				
+		assertFalse(savingsAccount.debit(0));
+		
+	}
+	
+	//------------------------------------------------------------------------------------
 
 }
